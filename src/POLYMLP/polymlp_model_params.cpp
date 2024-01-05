09ae785b3986dc41a13b00a3ea07cb9637177530
Atsuto Seko <sekocha@gmail.com>
2024-01-05 11:30:10 +0900

    updated

--- src/POLYMLP/polymlp_model_params.cpp
+++ src/POLYMLP/polymlp_model_params.cpp
@@ -1,26 +1,3 @@
-/****************************************************************************
-
-        Copyright (C) 2020 Atsuto Seko
-                seko@cms.mtl.kyoto-u.ac.jp
-
-        This program is free software; you can redistribute it and/or
-        modify it under the terms of the GNU General Public License
-        as published by the Free Software Foundation; either version 2
-        of the License, or (at your option) any later version.
-
-        This program is distributed in the hope that it will be useful,
-        but WITHOUT ANY WARRANTY; without even the implied warranty of
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-        GNU General Public License for more details.
-
-        You should have received a copy of the GNU General Public License
-        along with this program; if not, write to
-        the Free Software Foundation, Inc., 51 Franklin Street,
-        Fifth Floor, Boston, MA 02110-1301, USA, or see
-        http://www.gnu.org/copyleft/gpl.txt
-
-*****************************************************************************/
-
 #include "polymlp_model_params.h"
 
 ModelParams::ModelParams(){}
@@ -28,11 +5,54 @@ ModelParams::ModelParams(const struct feature_params& fp){
 
     n_type = fp.n_type;
 
-    // must be extended to more than ternary systems
     // Setting two elements in one array such as {0,1}, ... is unavailable.
     // If using such a setting, codes for computing features should be revised.
-    if (n_type == 1) type_comb_pair = {{{0}}};
-    else if (n_type == 2) type_comb_pair = {{{0}, {}}, {{1}, {0}}, {{}, {1}}};
+
+    if (n_type == 1) {
+        type_comb_pair = {{{0}}};
+    }
+    else if (n_type == 2) {
+        type_comb_pair = {{{0}, {}}, 
+                          {{1}, {0}}, 
+                          {{}, {1}}};
+    }
+    else if (n_type == 3) {
+        type_comb_pair = {{{0}, {}, {}}, 
+                          {{1}, {0}, {}}, 
+                          {{2}, {}, {0}}, 
+                          {{}, {1}, {}}, 
+                          {{}, {2}, {1}}, 
+                          {{}, {}, {2}}};
+    }
+    else if (n_type == 4) {
+        type_comb_pair = {{{0}, {}, {}, {}}, 
+                          {{1}, {0}, {}, {}}, 
+                          {{2}, {}, {0}, {}}, 
+                          {{3}, {}, {}, {0}}, 
+                          {{}, {1}, {}, {}}, 
+                          {{}, {2}, {1}, {}}, 
+                          {{}, {3}, {}, {1}}, 
+                          {{}, {}, {2}, {}}, 
+                          {{}, {}, {3}, {2}}, 
+                          {{}, {}, {}, {3}}};
+    }
+    else if (n_type == 5) {
+        type_comb_pair = {{{0}, {}, {}, {}, {}}, 
+                          {{1}, {0}, {}, {}, {}}, 
+                          {{2}, {}, {0}, {}, {}}, 
+                          {{3}, {}, {}, {0}, {}}, 
+                          {{4}, {}, {}, {}, {0}}, 
+                          {{}, {1}, {}, {}, {}}, 
+                          {{}, {2}, {1}, {}, {}}, 
+                          {{}, {3}, {}, {1}, {}}, 
+                          {{}, {4}, {}, {}, {1}}, 
+                          {{}, {}, {2}, {}, {}}, 
+                          {{}, {}, {3}, {2}, {}}, 
+                          {{}, {}, {4}, {}, {2}}, 
+                          {{}, {}, {}, {3}, {}},
+                          {{}, {}, {}, {4}, {3}},
+                          {{}, {}, {}, {}, {4}}};
+    }
 
     n_type_comb = type_comb_pair.size();
     initial_setting(fp);
@@ -87,6 +107,59 @@ ModelParams::ModelParams(const struct feature_params& fp, const bool icharge){
                               {{}, {}, {}, {3}, {}},
                               {{}, {}, {}, {4}, {3}},
                               {{}, {}, {}, {}, {4}}};
+        }
+        else if (n_type == 6) {
+            type_comb_pair = {{{0}, {}, {}, {}, {}, {}}, 
+                              {{1}, {0}, {}, {}, {}, {}}, 
+                              {{2}, {}, {0}, {}, {}, {}}, 
+                              {{3}, {}, {}, {0}, {}, {}}, 
+                              {{4}, {}, {}, {}, {0}, {}}, 
+                              {{5}, {}, {}, {}, {}, {0}}, 
+                              {{}, {1}, {}, {}, {}, {}}, 
+                              {{}, {2}, {1}, {}, {}, {}}, 
+                              {{}, {3}, {}, {1}, {}, {}}, 
+                              {{}, {4}, {}, {}, {1}, {}}, 
+                              {{}, {5}, {}, {}, {}, {1}}, 
+                              {{}, {}, {2}, {}, {}, {}}, 
+                              {{}, {}, {3}, {2}, {}, {}}, 
+                              {{}, {}, {4}, {}, {2}, {}}, 
+                              {{}, {}, {5}, {}, {}, {2}}, 
+                              {{}, {}, {}, {3}, {}, {}},
+                              {{}, {}, {}, {4}, {3}, {}},
+                              {{}, {}, {}, {5}, {}, {3}},
+                              {{}, {}, {}, {}, {4}, {}},
+                              {{}, {}, {}, {}, {5}, {4}},
+                              {{}, {}, {}, {}, {}, {5}}};
+        }
+        else if (n_type == 7) {
+            type_comb_pair = {{{0}, {}, {}, {}, {}, {}, {}}, 
+                              {{1}, {0}, {}, {}, {}, {}, {}}, 
+                              {{2}, {}, {0}, {}, {}, {}, {}}, 
+                              {{3}, {}, {}, {0}, {}, {}, {}}, 
+                              {{4}, {}, {}, {}, {0}, {}, {}}, 
+                              {{5}, {}, {}, {}, {}, {0}, {}}, 
+                              {{6}, {}, {}, {}, {}, {}, {0}}, 
+                              {{}, {1}, {}, {}, {}, {}, {}}, 
+                              {{}, {2}, {1}, {}, {}, {}, {}}, 
+                              {{}, {3}, {}, {1}, {}, {}, {}}, 
+                              {{}, {4}, {}, {}, {1}, {}, {}}, 
+                              {{}, {5}, {}, {}, {}, {1}, {}}, 
+                              {{}, {6}, {}, {}, {}, {}, {1}}, 
+                              {{}, {}, {2}, {}, {}, {}, {}}, 
+                              {{}, {}, {3}, {2}, {}, {}, {}}, 
+                              {{}, {}, {4}, {}, {2}, {}, {}}, 
+                              {{}, {}, {5}, {}, {}, {2}, {}}, 
+                              {{}, {}, {6}, {}, {}, {}, {2}}, 
+                              {{}, {}, {}, {3}, {}, {}, {}},
+                              {{}, {}, {}, {4}, {3}, {}, {}},
+                              {{}, {}, {}, {5}, {}, {3}, {}},
+                              {{}, {}, {}, {6}, {}, {}, {3}},
+                              {{}, {}, {}, {}, {4}, {}, {}},
+                              {{}, {}, {}, {}, {5}, {4}, {}},
+                              {{}, {}, {}, {}, {6}, {}, {4}},
+                              {{}, {}, {}, {}, {}, {5}, {}},
+                              {{}, {}, {}, {}, {}, {6}, {5}},
+                              {{}, {}, {}, {}, {}, {}, {6}}};
         }
         else {
             exit(8);
